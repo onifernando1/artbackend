7f2aa6d6551404baa68683b298d3e2f3c5144d2e
onifernando1 <onifernando1@gmail.com>
2025-05-28 16:51:55 +0100

    fix route urls

--- routes/paintings.js
+++ routes/paintings.js
@@ -6,18 +6,18 @@ const paintingController = require("../controllers/paintingController");
 // --- Define Routes ---
 
 // Post Method (e.g., POST /painting/post)
-router.post("/post", paintingController.postPainting);
+router.post("/", paintingController.postPainting);
 
 // Get All Method (e.g., GET /painting/getAll)
-router.get("/getAll", paintingController.getAllPaintings);
+router.get("/", paintingController.getAllPaintings);
 
 // Get by ID Method (e.g., GET /painting/getOne/123)
-router.get("/getOne/:id", paintingController.getPaintingById);
+router.get("/:id", paintingController.getPaintingById);
 
 // Update by ID Method (e.g., PATCH /painting/update/123)
-router.patch("/update/:id", paintingController.updatePaintingById);
+router.patch("/:id", paintingController.updatePaintingById);
 
 // Delete by ID Method (e.g., DELETE /painting/delete/123)
-router.delete("/delete/:id", paintingController.deletePaintingById);
+router.delete("/:id", paintingController.deletePaintingById);
 
 module.exports = router;
